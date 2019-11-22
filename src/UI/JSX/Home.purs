9de95bdf6d35bcb783d0bb883f5f94e00cb232d7
Asad Saeeduddin <masaeedu@gmail.com>
2019-11-22 00:29:42 -0500

    Use consistent formatting

--- src/UI/JSX/Home.purs
+++ src/UI/JSX/Home.purs
@@ -12,16 +12,21 @@ type HomePageData = { articles :: Array Article, tags :: Array Tag }
 
 banner :: JSX
 banner =
-  div |= cn "banner"
-  |- div |= cn "container"
+  div
+  |= cn "banner"
+  |- div
+     |= cn "container"
      |< [ h1 |= cn "logo-font" |- text "conduit"
         , p |- text "A place to share your knowledge."
         ]
 
 navItem :: Boolean -> String -> JSX -> JSX
 navItem active url content =
-  li |= cn "nav-item"
-  |- a |= cns classes |= href url
+  li
+  |= cn "nav-item"
+  |- a
+     |= cns classes
+     |= href url
      |- content
 
   where
@@ -29,8 +34,10 @@ navItem active url content =
 
 feedToggle :: JSX
 feedToggle =
-  div |= cn "feed-toggle"
-  |- ul |= cns ["nav", "nav-pills", "outline-active"]
+  div
+  |= cn "feed-toggle"
+  |- ul
+     |= cns ["nav", "nav-pills", "outline-active"]
      |< [ navItem false "" $ text "Your Feed"
         , navItem true  "" $ text "Global Feed"
         ]
@@ -38,19 +45,33 @@ feedToggle =
 -- variant 1
 articlePreview :: Article -> JSX
 articlePreview article =
-  div |= cn "article-preview"
-  |< [ div |= cn "article-meta"
-       |< [ a |= href article.author.url
+  div
+  |= cn "article-preview"
+  |< [ div
+       |= cn "article-meta"
+       |< [ a
+            |= href article.author.url
             |- img { src: article.author.picture }
-          , div |= cn "info"
-            |< [ a |= cn "author" |= href article.author.url
+          , div
+            |= cn "info"
+            |< [ a
+                 |= cn "author"
+                 |= href article.author.url
                  |- text article.author.name
-               , span |= cn "date"
+               , span
+                 |= cn "date"
                  |- text article.date
                ]
-          , button |= cns ["btn", "btn-outline-primary", "btn-sm", "pull-xs-right"]
+          , button
+            |= cns [ "btn"
+                   , "btn-outline-primary"
+                   , "btn-sm"
+                   , "pull-xs-right"
+                   ]
             |- i (cn "ion-heart") <> text (show article.hearts)
-          , a |= cn "preview-link" |= href article.url
+          , a
+            |= cn "preview-link"
+            |= href article.url
             |< [ h1 |- text article.title
                , p |- text article.description
                , span |- text "Read more..."
@@ -60,24 +81,35 @@ articlePreview article =
 
 tagPill :: Tag -> JSX
 tagPill tag =
-  a |= cns ["tag-pill", "tag-default"] |= href tag.url
+  a
+  |= cns ["tag-pill", "tag-default"]
+  |= href tag.url
   |- text tag.name
 
 tagList :: Array Tag -> JSX
 tagList ts =
-  div |= cn "tag-list"
+  div
+  |= cn "tag-list"
   |< map tagPill ts
 
 container :: HomePageData -> JSX
 container hpd =
-  div |= cns ["container", "page"]
-  |- div |= cn "row"
-     |- div |= cn "col-md-9"
-        |< ([ feedToggle ] <> map articlePreview hpd.articles <> [ tagList hpd.tags ])
+  div
+  |= cns ["container", "page"]
+  |- div
+     |= cn "row"
+     |- div
+        |= cn "col-md-9"
+        |< join
+           [ [feedToggle]
+           , map articlePreview hpd.articles
+           , [tagList hpd.tags]
+           ]
 
 home :: HomePageData -> JSX
 home hpd =
-  div |= cn "home-page"
+  div
+  |= cn "home-page"
   |< [ banner
      , container hpd
      ]
--- src/UI/JSX/Nav.purs
+++ src/UI/JSX/Nav.purs
@@ -2,35 +2,36 @@ module Nav where
 
 import Prelude
 
-import React.Basic as R
-import React.Basic.DOM as R
+import Common (cn, href)
+import React.Basic (JSX)
+import React.Basic.DOM (a, div, i, li, nav, text, ul)
 import Snap.React.Component ((|-), (|<), (|=))
 
-navItem :: Boolean -> String -> R.JSX -> R.JSX
+navItem :: Boolean -> String -> JSX -> JSX
 navItem active href content =
-  R.li
-  |= { className: "nav-item" }
-  |- R.a 
+  li
+  |= cn "nav-item"
+  |- a
      |= { className, href }
      |- content
 
   where
   className = "nav-link" <> if active then " active" else ""
 
-nav :: R.JSX
-nav =
-  R.nav
-  |= { className: "navbar navbar-light" }
-  |- R.div
-     |= { className: "container" }
-     |< [ R.a 
-          |= { className: "navbar-brand", href: "index.html" }
-          |- R.text "conduit"
-        , R.ul
-          |= { className: "nav navbar-nav pull-xs-right" }
-          |< [ navItem true "" $ R.text "Home"
-             , navItem false "" $ R.i { className: "ion-compose" } <> R.text "&nbsp;New Post"
-             , navItem false "" $ R.i { className: "ion-gear-a" } <> R.text "&nbsp;Settings"
-             , navItem false "" $ R.text "Sign up"
+navbar :: JSX
+navbar =
+  nav
+  |= cn "navbar navbar-light"
+  |- div
+     |= cn "container"
+     |< [ a
+          |= cn "navbar-brand" |= href "index.html"
+          |- text "conduit"
+        , ul
+          |= cn "nav navbar-nav pull-xs-right"
+          |< [ navItem true "" $ text "Home"
+             , navItem false "" $ i (cn "ion-compose") <> text "&nbsp;New Post"
+             , navItem false "" $ i (cn "ion-gear-a") <> text "&nbsp;Settings"
+             , navItem false "" $ text "Sign up"
              ]
         ]
