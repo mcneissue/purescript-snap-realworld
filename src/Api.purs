d06ce1b5800a8b0193bd752e21d9e0a09259151d
John C. Lavelle <jlavelle604@gmail.com>
2019-11-22 12:50:37 -0500

    Handle Articles GET params

--- src/Api.purs
+++ src/Api.purs
@@ -8,11 +8,22 @@ import Affjax.ResponseFormat as ResponseFormat
 import Control.Monad.Reader.Class (class MonadAsk, asks)
 import Data.Bifunctor (lmap)
 import Data.Either (Either)
+import Data.Filterable (compact, filter)
+import Data.FoldableWithIndex (foldMapWithIndex, foldlWithIndex)
+import Data.Lens.Record (prop)
+import Data.Lens.Setter ((%~))
+import Data.List (List(..), catMaybes, fromFoldable, intercalate, (:))
 import Data.List.Types (NonEmptyList)
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Maybe (Maybe(..), isJust)
 import Data.Newtype (class Newtype, unwrap)
+import Data.Symbol (SProxy(..))
+import Data.Tuple (Tuple(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Foreign (ForeignError)
 import Model (Article)
+import Record.ToMap (rlToMap)
 import Simple.JSON as JSON
 
 newtype Url = Url String
@@ -43,14 +54,38 @@ parseGet :: forall m r a
          -> m (Either ApiError a)
 parseGet endpoint parser = map (_ >>= parser <<< _.body) $ lmap AffjaxError <$> get ResponseFormat.string endpoint
 
+type GetArticlesParams = 
+  { tag :: Maybe String
+  , author :: Maybe String
+  , favorited :: Maybe String
+  , limit :: Maybe Int
+  , offset :: Maybe Int
+  }
+
 getArticles :: forall m r
             . MonadAsk { apiUrl :: Url | r } m 
            => MonadAff m 
-           => m (Either ApiError (Array Article))
-getArticles = parseGet "/articles/"  (map _.articles <<< articlesParser)
+           => GetArticlesParams
+           -> m (Either ApiError (Array Article))
+getArticles p = parseGet (buildQuery paramList "/articles") (map _.articles <<< articlesParser)
+  where
+  paramList = compact $ rlToMap $ p # _limit %~ map show
+                                    # _offset %~ map show
+  _limit = prop (SProxy :: SProxy "limit")
+  _offset = prop (SProxy :: SProxy "offset")
 
 articlesParser :: String -> Either ApiError { articles :: Array Article }
 articlesParser = readJson
+
+buildQuery :: Map String String -> String -> String
+buildQuery params url | Map.isEmpty params = url
+                      | otherwise = url <> "?" <> foldMapWithIndex go unfolded
+  where
+  unfolded :: Array (Tuple String String)
+  unfolded = Map.toUnfoldable params
+
+  go 0 (Tuple k v) = k <> "=" <> v
+  go n (Tuple k v) = "&" <> k <> "=" <> v
 
 readJson :: forall a. JSON.ReadForeign a => String -> Either ApiError a
 readJson = lmap ParseError <<< JSON.readJSON
--- src/Main.purs
+++ src/Main.purs
@@ -3,10 +3,11 @@ module Main where
 import Prelude
 
 import Affjax as AX
-import Api (getArticles, Url(..))
+import Api (Url(..), GetArticlesParams, getArticles)
 import Control.Monad.Reader (runReaderT)
 import Data.Argonaut as J
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (launchAff)
 import Effect.Class.Console (logShow, log)
@@ -14,9 +15,18 @@ import Effect.Class.Console (logShow, log)
 testUrl :: Url
 testUrl = Url "https://conduit.productionready.io/api"
 
+params :: GetArticlesParams
+params =
+  { tag: Just "foo"
+  , author: Nothing
+  , favorited: Nothing
+  , limit: Just 1
+  , offset: Nothing
+  }
+
 main :: Effect Unit
 main = void $ launchAff $ do
-  result <- runReaderT getArticles { apiUrl: testUrl} 
+  result <- runReaderT (getArticles params) { apiUrl: testUrl} 
   case result of
     Right r -> logShow r
     Left e  -> logShow e
